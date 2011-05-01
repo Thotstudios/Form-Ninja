043ce88f510185655c5dcc30ca0fe798bc4cfb49
Chad Hatcher <hackenslacker@gmail.com>
2011-04-30 18:51:45 -0700

    ok

--- Form Ninja/mjbhvg.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  mjbhvg.cpp
-//  Form Ninja
-//
-//  Created by Hackenslacker on 4/30/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
-
-#include "mjbhvg.h"
--- Form Ninja/test.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  test.cpp
-//  Form Ninja
-//
-//  Created by Hackenslacker on 4/30/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
-
-#include "test.h"
