ba5993396d3de40e9f342714beaa4d8964066ab1
Chad Hatcher <hackenslacker@gmail.com>
2011-05-13 13:04:16 -0700

    Added TemplateManagerViewController

---
+++ FormNinja/TemplateManager/TemplateManagerViewController.h
@@ -0,0 +1,16 @@
+//
+//  TemplateManagerViewController.h
+//  FormNinja
+//
+//  Created by Hackenslacker on 5/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface TemplateManagerViewController : UIViewController {
+    
+}
+
+@end
