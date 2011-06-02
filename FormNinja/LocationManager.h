c8a09102be7a263276833fa1a4bee9a0c7782068
Paul Salazar <psalazar@thotstudios.com>
2011-06-02 13:48:35 -0700

    Added core location framework and location class (implemetation to come).

---
+++ FormNinja/LocationManager.h
@@ -0,0 +1,17 @@
+//
+//  LocationManager.h
+//  FormNinja
+//
+//  Created by Ollin on 6/2/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+
+
+@interface LocationManager : NSObject <CLLocationManagerDelegate>{
+    
+}
+
+@end
---
+++ FormNinja/LocationManager.m
@@ -0,0 +1,14 @@
+//
+//  LocationManager.m
+//  FormNinja
+//
+//  Created by Ollin on 6/2/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "LocationManager.h"
+
+
+@implementation LocationManager
+
+@end
