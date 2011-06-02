8790de0a498c108db7108727f4d8d5b155c5077e
Paul Salazar <psalazar@thotstudios.com>
2011-06-02 15:23:11 -0700

    Initial LocationManager class implementation.

--- FormNinja/LocationManager.h
+++ FormNinja/LocationManager.h
@@ -11,7 +11,23 @@
 
 
 @interface LocationManager : NSObject <CLLocationManagerDelegate>{
+    CLLocationManager* locationManager;
     
+    BOOL locationDefined;
+    BOOL locationDenied;
+
+	float latitude;
+	float longitude;
 }
+
+@property (readonly, nonatomic) float latitude, longitude;
+@property (readonly, nonatomic) BOOL locationDefined, locationDenied;
+
+
++ (LocationManager*)locationManager;
+
+- (void) startUpdates;
+- (void) stopUpdates;
+- (BOOL) locationServicesEnabled;
 
 @end
