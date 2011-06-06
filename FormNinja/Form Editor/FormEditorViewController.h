bfaa38c161f8d50223429d55ed2f4b73a90be13a
Ron Lugge <rlugge@thotstudios.com>
2011-06-06 11:16:12 -0700

    Some quick work on creating new form editor.  Only field showing up is the metadata one, unsure why.

---
+++ FormNinja/Form Editor/FormEditorViewController.h
@@ -0,0 +1,17 @@
+//
+//  FormEditorViewController.h
+//  FormNinja
+//
+//  Created by Programmer on 6/6/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#include "TemplateEditorController.h"
+
+
+@interface FormEditorViewController : TemplateEditorController {
+    
+}
+
+@end
