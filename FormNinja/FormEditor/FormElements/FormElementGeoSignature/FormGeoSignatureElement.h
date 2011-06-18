f855bf295c6606f034b45907ed350952b5bf70d6
Ron Lugge <rlugge@thotstudios.com>
2011-06-18 15:25:47 -0700

    Renamed FormGeoSignature to follow naming conventions, so it's now FormGeoSignatureElement

---
+++ FormNinja/FormEditor/FormElements/FormElementGeoSignature/FormGeoSignatureElement.h
@@ -0,0 +1,17 @@
+//
+//  FormGeoSignature.h
+//  FormNinja
+//
+//  Created by Hackenslacker on 6/18/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GeoSignatureElement.h"
+
+@interface FormGeoSignatureElement : GeoSignatureElement {
+    
+}
+
+@end
